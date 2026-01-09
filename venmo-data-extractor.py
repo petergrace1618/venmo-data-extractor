ea741cf2e4161804f55f59cb79585639febdbc1b
Peter Grace <petergrace1.618@gmail.com>
2026-01-08 23:16:05 -0800

    Rename variables in __main__

--- venmo-data-extractor.py
+++ venmo-data-extractor.py
@@ -16,7 +16,7 @@ class VenmoDialect(csv.excel):
         setattr(self, 'strict', True)
 
 
-def get_fieldnames(fname):
+def get_fieldnames(fname) -> list[str]:
     # ignore first two lines, header is on third
     with open(fname, encoding='utf-8') as f:
         f.readline()
@@ -46,36 +46,35 @@ if __name__ == '__main__':
     csv_files_dir = Path('./csv')
     csv_files = list(csv_files_dir.glob('VenmoStatement*.csv'))
     output_file = 'Venmo_transactions.txt'
+    name = 'Amanda Ruiz'
 
     if len(csv_files) == 0:
         print('No .csv files in', csv_files_dir)
-        exit()
+        exit(1)
 
     # Field names:
     # ,ID,Datetime,Type,Status,Note,From,To,Amount (total),Amount (tip),
     # Amount (tax),Amount (fee),Tax Rate,Tax Exempt,Funding Source,
     # Destination,Beginning Balance,Ending Balance,Statement Period Venmo Fees,
     # Terminal Location,Year to Date Venmo Fees,Disclaimer
-
-    # get field names
     fieldnames = get_fieldnames(csv_files[0])
 
-    name = 'Amanda Ruiz'
-
-    # lines is a 2D array of strings that holds all transactions between G and I
-    lines = [fieldnames]
+    # transactions: list[list[str]]
+    # first row of transactions is header
+    # the rest will be transactions between G and I
+    transactions = [fieldnames]
     for csv_file in csv_files:
         with open(csv_file, newline='', encoding='utf-8') as f:
             csv_reader = csv.reader(f, dialect=VenmoDialect)
-            for line in csv_reader:
-                if name in line:
-                    lines.append(line)
+            for transaction in csv_reader:
+                if name in transaction:
+                    transactions.append(transaction)
 
 
-    # Get indices of output fields
+    # Get indices of output fields: [2, 6, 7, 8, 5]
     output_fields = ['Datetime', "From", 'To', 'Amount (total)', 'Note']
     columns = [fieldnames.index(field) for field in output_fields]
-    # columns = [2, 6, 7, 8, 5]
+    output_header = format_line(transactions[0], columns)
 
     doc = 'VENMO TRANSACTIONS BETWEEN @PETER-GRACE-16 AND @AMANDA-RUIZ-139\n'
     doc += 'FROM FEBRUARY 2023 TO NOVEMBER 2024\n'
@@ -83,14 +82,12 @@ if __name__ == '__main__':
     doc += '(For source code and data see:\n'
     doc += 'https://github.com/petergrace1618/venmo-data-extractor.git)\n'
     doc += '\n'
-    doc += format_line(lines[0], columns)
+    doc += output_header
     doc += '-' * 64 + '\n'
 
-    for line in lines[1:]:
-        doc += format_line(line, columns)
+    for transaction in transactions[1:]:
+        doc += format_line(transaction, columns)
 
-    # print(summary)
-    # Write summary to file
     with open(output_file, 'w', encoding='utf-8') as o:
         o.write(doc)
 
